55cbd74306204a4328cd9d4138066bd7241d375d
Paulo Lima <47723610+prmlimajr@users.noreply.github.com>
2020-06-04 20:19:59 -0300

    Backend concluído

---
+++ server/src/server.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import path from 'path';
+import cors from 'cors';
+import routes from './routes';
+
+const port = 3333;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
+app.listen(port, ()=>{
+  console.log(`Conectado na porta ${port}.`)
+});
