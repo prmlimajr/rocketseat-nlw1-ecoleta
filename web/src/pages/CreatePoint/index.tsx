5cd1c456a6edfa7db32573db70c93486a2534387
Paulo Lima <47723610+prmlimajr@users.noreply.github.com>
2020-06-08 01:18:38 -0300

    Add files via upload

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -7,6 +7,7 @@ import { Map, TileLayer, Marker } from 'react-leaflet';
 import api from '../../services/api';
 import axios from 'axios';
 import { LeafletMouseEvent } from 'leaflet';
+import Dropzone from '../../components/Dropzone';
 
 interface Item {
   id: number;
@@ -37,6 +38,7 @@ const CreatePoint = () =>{
   });
   const [ selectedItems, setSelectedItems ] = useState<number[]>([]);
   const history = useHistory();
+  const [ selectedFile, setSelectedFile ] = useState<File>();
 
   useEffect(()=>{
     navigator.geolocation.getCurrentPosition(position => {
@@ -112,16 +114,22 @@ const CreatePoint = () =>{
     const [ latitude, longitude ] = selectedPosition;
     const items = selectedItems;
     
-    const data = {
-      name, 
-      email, 
-      whatsapp,
-      uf,
-      city,
-      latitude,
-      longitude,
-      items
-    };
+
+    const data = new FormData();
+    
+      data.append('name', name);
+      data.append('email', email); 
+      data.append('whatsapp', whatsapp);
+      data.append('uf', uf);
+      data.append('city', city);
+      data.append('latitude', String(latitude));
+      data.append('longitude', String(longitude));
+      data.append('items', items.join(','));
+      
+      if(selectedFile){
+        data.append('image', selectedFile);
+      }
+    
     await api.post('points', data);
 
     alert("Ponto de coleta criado!");
@@ -140,6 +148,8 @@ const CreatePoint = () =>{
       
       <form onSubmit={handleSubmit}>
         <h1>Cadastro do <br />ponto de coleta</h1>
+
+        <Dropzone onFileUploaded={setSelectedFile} />
 
         <fieldset>
           <legend>
